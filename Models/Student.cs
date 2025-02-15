cb6a7b6db7ca9c31dfcc529a25a547931eab930e
tasosger <tassos.gerassis@gmail.com>
2025-02-15 16:39:36 +0200

    add Student class

---
+++ Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UniPath_MVC.Models
+{
+    
+    public class Student: User
+    {
+        public string? LevelOfEducation { get; set; }
+
+    }
+}
