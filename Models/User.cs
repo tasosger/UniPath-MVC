ad9e8393352a71de15830d0f66560db3d8b234d9
tasosger <tassos.gerassis@gmail.com>
2025-02-15 16:29:45 +0200

    User model

---
+++ Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniPath_MVC.Models
+{
+
+    public abstract class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Username { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password;
+
+        [Required]
+        public string? FirstName { get; set; }
+
+        [Required]
+        public string? LastName { get; set; }
+
+        public string? Bio { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+          
+
+    }
+}
