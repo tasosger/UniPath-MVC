cf2c17282b4698b9be70c9411c20b3b2bfa6f97a
tasosger <tassos.gerassis@gmail.com>
2025-02-15 17:23:56 +0200

    Class and Capsule Models

---
+++ Models/Capsule.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace UniPath_MVC.Models
+{
+    public class Capsule
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required]
+        public string? Title { get; set; }
+
+        public string? VideoURL { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Content { get; set; }
+
+        public int ClassId { get; set; }
+        [ForeignKey("ClassId")]
+        public Class? Class { get; set; }
+        public Capsule(string title, int classId, string? description = null, string? content = null, string? videoURL = null)
+        {
+            Title = title;
+            ClassId = classId;
+            Description = description;
+            Content = content;
+            VideoURL = videoURL;
+        }
+
+        public Capsule() { 
+        
+        }
+    }
+}
---
+++ Models/Class.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UniPath_MVC.Models
+{
+    public class Class
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public int TeacherId { get; set; }
+        [ForeignKey("TeacherId")]
+        public Teacher? Teacher { get; set; }
+
+        public ICollection<Capsule>? Capsules { get; set; }
+        public Class(string name, int teacherId, string? description = null)
+        {
+            Name = name;
+            TeacherId = teacherId;
+            Description = description;
+            Capsules = new List<Capsule>(); // When a Class is created no capsules belong to the class
+        }
+        public Class() { 
+        
+        }
+    }
+}
