e2fd6b6a7b563f154f9ea035cd0531cad23a7957
tasosger <tassos.gerassis@gmail.com>
2025-02-15 17:26:13 +0200

    true false question for capsule model

---
+++ Models/TrueFalseQuestion.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UniPath_MVC.Models
+{
+    public class TrueFalseQuestion
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Question { get; set; }
+
+        [Required]
+        public bool? CorrectAnswer { get; set; } 
+
+        public int CapsuleId { get; set; }
+        [ForeignKey("CapsuleId")]
+        public Capsule? Capsule { get; set; }
+
+        public TrueFalseQuestion(string question, bool correctAnswer, int capsuleId)
+        {
+            Question = question;
+            CorrectAnswer = correctAnswer;
+            CapsuleId = capsuleId;
+        }
+
+        public TrueFalseQuestion() { }
+    }
+}
