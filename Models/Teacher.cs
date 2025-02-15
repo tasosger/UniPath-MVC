aaa6ebda09f02ab921d7a0c2999b9da8b53bba36
tasosger <tassos.gerassis@gmail.com>
2025-02-15 16:41:45 +0200

    Teacher clas

---
+++ Models/Teacher.cs
@@ -0,0 +1,9 @@
+﻿namespace UniPath_MVC.Models
+{
+    public class Teacher: User
+    {
+        public string? Specialization { get; set; }
+        public string? University { get; set; }
+        public string? Hierarchy { get; set; }
+    }
+}
