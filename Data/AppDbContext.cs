eccfb0ef4fcbff324e51e840aca5800985c18851
tasosger <tassos.gerassis@gmail.com>
2025-02-18 10:24:16 +0200

    Db model

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniPath_MVC.Models;
+
+namespace UniPath_MVC.Data
+
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Capsule> Capsules { get; set; }
+        public DbSet<TrueFalseQuestion> TrueFalseQuestions { get; set; }
+
+    }
+}
