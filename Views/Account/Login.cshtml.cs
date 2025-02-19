f5ad92404de52a73531fd642293ccb12b3eb5913
tasosger <tassos.gerassis@gmail.com>
2025-02-19 23:57:39 +0200

    login view

---
+++ Views/Account/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UniPath_MVC.Views.Account
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
