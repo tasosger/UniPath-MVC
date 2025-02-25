74b3c50d97bbc25b914f3146d6edee8c863d4e01
tasosger <tassos.gerassis@gmail.com>
2025-02-25 10:18:42 +0200

    repalce map static assets to ensure compatibility

--- Program.cs
+++ Program.cs
@@ -63,13 +63,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
+app.UseStaticFiles();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-.WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
 
